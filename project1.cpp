3e3555c00920499389a5aa6cbc32c740ccdb9704
Pierre Coric <pierrecoric@Elizas-MBP.lan>
2025-02-01 19:17:59 +0100

    project1

--- project1.cpp
+++ project1.cpp
@@ -13,19 +13,18 @@ object of the class VectorDouble and there is no more room, then a new
 dynamic array with twice the capacity of the old dynamic array is created
 and the values of the old dynamic array are copied to the new dynamic array.Programming Projects
 Your class should have all of the following:
-■	Three constructors: a default constructor that creates a dynamic array
+■Three constructors: a default constructor that creates a dynamic array
 for 50 elements, a constructor with one int argument for the number
 of elements in the initial dynamic array, and a copy constructor.
-■	A destructor.
-■
-A suitable overloading of the assignment operator =.
-■	A suitable overloading of the equality operator ==. To be equal, the
+■A destructor.
+■A suitable overloading of the assignment operator =.
+■A suitable overloading of the equality operator ==. To be equal, the
 values of count and the count array elements must be equal, but the
 values of max_count need not be equal.
-■	Member functions push_back, capacity, size, reserve, and resize
+■Member functions push_back, capacity, size, reserve, and resize
 that behave the same as the member functions of the same names for
 vectors.
-■	Two member functions to give your class the same utility as the
+■Two member functions to give your class the same utility as the
 square brackets: value_at(i), which returns the value of the ith
 element in the dynamic array; and change_value_at(d, i), which
 changes the double value at the ith element of the dynamic array
@@ -34,3 +33,73 @@ change_value_at. (Your class will not work with the square brackets.
 It can be made to work with square brackets, but we have not covered
 the material which tells you how to do that.)
 */
+
+#include <iostream>
+using std::cout;
+
+class VectorDouble {
+    private:
+        double* value;
+        int maxCount;
+        int count;
+    public:
+        VectorDouble();
+        VectorDouble(int elements);
+        VectorDouble(VectorDouble& original);
+        ~VectorDouble();
+        VectorDouble operator =(VectorDouble& original);
+        friend bool operator ==(VectorDouble& vectorA, VectorDouble& vectorB);
+        void pushBack(double n);
+        int capacity() const {return maxCount;}
+        int size() const {return count;}
+        void reserve(int size);
+        void resize(int newSize);
+        double valueAt(int i) {return value[i];}
+        void changeValueAt(double n, int i) {value[i] = n;}
+};
+
+VectorDouble::VectorDouble() {
+    value = new double;
+    maxCount = 0;
+    count  = 0;
+}
+
+VectorDouble::VectorDouble(int elements) {
+    value = new double[elements];
+    maxCount = elements;
+    count = 0;
+}
+
+
+void VectorDouble::pushBack(double n) {
+    if(count < maxCount) {
+        value[count] = n;
+        count ++;
+    }
+    else {
+        resize(2 * maxCount);
+        pushBack(n);
+    }
+}
+
+void VectorDouble::reserve(int size) {
+    if((maxCount - count) < size) {
+        resize(size);
+    }
+}
+
+void VectorDouble::resize(int newSize) {
+    double* temp;
+    temp = new double[newSize];
+    for(int i = 0; i < count; i++) {
+        temp[i] = value[i];
+    }
+    delete value;
+    value = temp;
+    maxCount = newSize;
+}
+
+int main() {
+    VectorDouble(10);
+    return 0;
+}
